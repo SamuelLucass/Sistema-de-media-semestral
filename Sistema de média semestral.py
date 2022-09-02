816080ac7ad679fbf537b929964bfc5a020ace52
SamuelLucass <Samuka.silva@outlook.com>
2022-09-02 09:41:52 -0300

    Sistemas de Média semestral

---
+++ Sistema de média semestral.py
@@ -0,0 +1,13 @@
+#Coleta de dados de nota dos alunos
+Num1 = float(input("Digite a nota do primeiro semestre: "))
+Num2 = float(input("Digite a nota do segundo semestre: "))
+Num3 = float(input("Digite a nota do terceiro semestre: "))
+Num4 = float(input("Digite a nota do quarto semestre: "))
+
+#equação para cehagar a média dos alunos
+Soma = Num1 + Num2 + Num3 + Num4
+Media = Soma / 4
+
+#imprime para o usuário a soma e a media usando o operador %
+print("A soma entre:", Num1, ",", Num2, ",", Num3, "e", Num4,  "É igual a: %s"  %(Soma))
+print("A média do aluno durante o semestre foi: %s" %(Media))
